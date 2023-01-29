f1519133c9ee7371a6ec4df0e6778532c9f3bd46
ashok <ashok@gmail.com>
2023-01-29 18:48:29 +0530

    Layered APP-Multiple DBs

---
+++ BootProj04-LayeredApp-RealtimeDI-ForMultipleDBs/src/main/java/com/nt/model/Employee.java
@@ -0,0 +1,16 @@
+//Employee.java
+package com.nt.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+	private Integer empno;
+	private  String ename;
+	private  String  desg;
+	private  Double salary;
+	private  Double grossSalary;
+	private  Double netSalary;
+	
+
+}
