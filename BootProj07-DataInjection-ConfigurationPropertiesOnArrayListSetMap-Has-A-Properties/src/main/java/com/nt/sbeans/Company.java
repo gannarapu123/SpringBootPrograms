c6f1f989f082a09595a4168edaa4f9dc5003fb11
ashok <ashok@gmail.com>
2023-01-29 18:50:18 +0530

    ConfigurationProperties on multiple properties

---
+++ BootProj07-DataInjection-ConfigurationPropertiesOnArrayListSetMap-Has-A-Properties/src/main/java/com/nt/sbeans/Company.java
@@ -0,0 +1,13 @@
+package com.nt.sbeans;
+
+import lombok.Data;
+
+@Data
+public class Company {
+	private Integer id;
+	private String name;
+	private Integer size;
+	private String addrs;
+	
+
+}
---
+++ BootProj07-DataInjection-ConfigurationPropertiesOnArrayListSetMap-Has-A-Properties/src/main/java/com/nt/sbeans/Employee.java
@@ -0,0 +1,30 @@
+package com.nt.sbeans;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+@Component("emp")
+@Data
+@ConfigurationProperties(prefix="emp.info")
+public class Employee {
+	//simple properties
+	private Integer eno;
+	private String ename;
+	
+	//array type
+	private String[] favColors;
+	
+	//collection type
+	private List<String> nickNames;
+	private Set<Long> phoneNumbers;
+	private Map<String,Object> idDetails;
+	
+	//HAS-A property
+	private Company company;
+
+}
